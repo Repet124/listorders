a55ce685eaca441d9604fb8a6e0b68de31f5a454
Oleg <hlop4ik124@gmail.com>
2019-09-24 00:51:45 +0300

    Финальная подчистка удалены ненужные файлы, написан README

---
+++ build/precache-manifest.5f4d09860eca815401e109829d8233a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f346754d88665a28df225210029fadd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca7e124a3073b2d5e522",
+    "url": "/static/css/main.ba731b19.chunk.css"
+  },
+  {
+    "revision": "cd228eb7e5eb1dac5dc0",
+    "url": "/static/js/2.aba644ab.chunk.js"
+  },
+  {
+    "revision": "ca7e124a3073b2d5e522",
+    "url": "/static/js/main.b413f976.chunk.js"
+  },
+  {
+    "revision": "bfad9176e15ebd40b9f7",
+    "url": "/static/js/runtime-main.8e991451.js"
+  }
+]);
